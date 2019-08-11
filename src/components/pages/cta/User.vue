6a93db108b3b50671ea414a216ce80241a006d8c
Nikolay <nikolov_n_s@abv.bg>
2019-08-11 12:30:54 +0300

    Show user tabs: Details, Products, Orders. Show content for each tab on click

---
+++ src/components/pages/cta/User.vue
@@ -0,0 +1,129 @@
+<template>
+    <div class="user-container row">
+        
+        <div class="page-title col-md-12">
+            <h1>User profile</h1>
+        </div>
+        <div class="col-md-12">
+            <ul class="nav nav-tabs">
+                <li 
+                    class="nav-item"
+                    v-for="(category, index) in categories"
+                    :key="index"
+                    @click="selectedCategory = category"
+                    >
+                    <a :class="{ 'active': selectedCategory == category }" class="nav-link">{{ category }}</a>
+                </li>
+            </ul>
+            <div class="tab-content">
+                <template v-if="showSelectedTab('Details')">
+                    <div id="details">
+                        <p>details</p>
+                    </div>
+                </template>
+
+                <template v-if="showSelectedTab('Products')">
+                    <div id="products">
+                        <p>Products</p>
+                    </div>
+                </template>
+
+                <template v-if="showSelectedTab('Orders')">
+                    <div id="orders">
+                        <p>Orders</p>
+                    </div>
+                </template>
+                
+            </div>
+        </div>
+        
+    </div>
+</template>
+
+<script>
+// import UserLogin from './UserLogin'
+// import UserRegister from './UserRegister'
+// import UserDetails from './UserDetails'
+// import UserProducts from './UserProducts'
+
+export default {
+    data() {
+        return {
+            selectedCategory: 'Details',
+            categories: ['Details', 'Products', 'Orders'],
+            userData: [
+                {'name': 'John Doe', 'email': 'john@mail.com', 'web': 'the-best.com', 'phone': '8888 888 888', 'address': "John's city" },
+            ],
+            userProducts: [
+                {"id":"1", 
+                "type":"1", 
+                "name":"John Doe", 
+                "position":"Mnager", 
+                "company":"John's Company", 
+                "phone":"0888 888 888", 
+                "address":"John's street", 
+                "email":"john@gmail.com", 
+                "web":"www.the-best.com",
+                "logo":"logo",
+                "background":"#ccc",
+                "image":"image"
+                },
+                {"id":"3", 
+                "type":"1", 
+                "name":"John Doe", 
+                "position":"Mnager", 
+                "company":"John's Company", 
+                "phone":"0888 888 888", 
+                "address":"John's street", 
+                "email":"john@gmail.com", 
+                "web":"www.the-best.com",
+                "logo":"logo",
+                "background":"#ccc",
+                "image":"image 2"
+                },
+                {"id":"2", 
+                "type":"2", 
+                "name":"John Doe", 
+                "position":"Mnager", 
+                "company":"John's Company", 
+                "phone":"0888 888 888", 
+                "address":"John's street", 
+                "email":"john@gmail.com", 
+                "web":"www.the-best.com",
+                "logo":"logo 2",
+                "background":"#ddd",
+                "image":"image 2"
+                }
+            ],
+            userOrders: [
+                {"userId": "1",
+                "id":"1",
+                "productId":"1",
+                "productName":"Name 1",
+                "image": "Image 1",
+                "userName": "John Doe",
+                "phone":"0888 888 888",
+                "address":"Jhon's street",
+                'quantity': 100
+                },
+                {"userId": "1",
+                "id":"2",
+                "productId":"3",
+                "productName":"Name 3",
+                "image": "Image 3",
+                "userName": "John Doe",
+                "phone":"0888 888 888",
+                "address":"Jhon's street",
+                'quantity': 500
+                }
+            ]
+        }
+    },
+    methods: {
+        showSelectedTab(category) {
+            return this.selectedCategory == category;
+        }
+    }
+}
+</script>
+
--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,13 @@
 import Home from './components/pages/simple/Home.vue'
 import About from './components/pages/simple/About.vue'
 import Templates from './components/pages/simple/Templates.vue'
+import User from './components/pages/cta/User.vue'
+import Register from './components/pages/simple/Register.vue'
 
 
 export const routes = [
     { path: '', component: Home },
     { path: '/about', component: About },
-    { path: '/templates', component: Templates }
+    { path: '/templates', component: Templates },
+    { path: '/user', component: User }
 ]
